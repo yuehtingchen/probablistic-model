4ded64f4f553f786802893b07b2dde04015f7170
yuehtingchen <alicetingchen@gmail.com>
2024-06-29 16:55:26 -0700

    revert changes of making genes conditionally independent

--- LogNorm.py
+++ LogNorm.py
@@ -46,11 +46,10 @@ class LogNorm(nn.Module):
 
 			state = self.dayToState(patient_data_day[s], patient_data_endo[s])
 
-			with pyro.plate("genes_{}".format(s), X.shape[1]):
-				if self.shift:
-					pyro.sample("X_{}".format(s), pyro.distributions.Normal(mu[state] + S, sigma[state]), obs=(X[s]) if X is not None else None)
-				else:
-					pyro.sample("X_{}".format(s), pyro.distributions.Normal(mu[state], sigma[state]), obs=(X[s]) if X is not None else None)
+			if self.shift:
+				pyro.sample("X_{}".format(s), pyro.distributions.Normal(mu[state] + S, sigma[state]).to_event(1), obs=(X[s]) if X is not None else None)
+			else:
+				pyro.sample("X_{}".format(s), pyro.distributions.Normal(mu[state], sigma[state]).to_event(1), obs=(X[s]) if X is not None else None)
 
 
 	def dayToState(self, day, endo=None):
